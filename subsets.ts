c2991266f4dfbbbf8382649eb6452a1b395ac6c2
natlou <natloudev@gmail.com>
2022-08-31 15:19:41 -0700

    templating subset

---
+++ subsets.ts
@@ -0,0 +1,9 @@
+function subsets(nums: number[]): number[][] {
+    // each element is a new level 
+//                                [1,2,3]
+// is 1 in the set (y/n)   [1,2,3]       [2, 3]
+// is 2 in the set (y/n)[1,2,3] [1,3]  [2,3] [3]
+// is 3 in the set [1,2,3] [1,2] [1,3][2,3][2][3][]
+    let output = [];
+    return output;
+};
