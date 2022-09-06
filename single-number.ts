bdd8a3a0b0a585ab339e14cb23a549c0dcafe906
natlou <natloudev@gmail.com>
2022-09-05 21:26:32 -0700

    single number hashmap

--- single-number.ts
+++ single-number.ts
@@ -1,4 +1,4 @@
-function singleNumber(nums: number[]): number {
+function singleNumberHashSet(nums: number[]): number {
     
     let hashset = new Set(); 
     let unique = nums.filter( (item, index, arr) => arr.indexOf(item) === index); 
@@ -14,4 +14,25 @@ function singleNumber(nums: number[]): number {
 
     return unique[0];
     
+};
+
+function singleNumberHashMap(nums: number[]): number {
+    
+    let hashmap = new Map(); 
+    
+    for (let num of nums) {
+        if (!hashmap.has(num)) {
+            hashmap.set(num, 1); 
+        } else {
+            hashmap.set(num, 2);
+        }
+    }
+    
+    for (let [key, value] of hashmap) {
+        if (value === 1) {
+            return key;
+        } 
+    }
+    
+    return -1;
 };
